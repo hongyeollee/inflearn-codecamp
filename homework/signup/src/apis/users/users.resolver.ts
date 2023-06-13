27d0aaa0013b26fe86ee0c49fd42152781b0895a
이홍열 <zeler1004@gmail.com>
2023-06-13 13:31:27 +0900

    [add] signupCRUD - update부분은 보완해야할 필요가 있음 - 차후 이 커밋에 대한 내용을 확인하게 되면 update 기능을 개선할 필요가 있음

--- homework/signup/src/apis/users/users.resolver.ts
+++ homework/signup/src/apis/users/users.resolver.ts
@@ -1,12 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './users.service';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+import { UpdateResult } from 'typeorm';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Query(() => String)
+  hello(): string {
+    return 'Hello World!';
+  }
+
+  @Mutation(() => User, { nullable: true })
+  createUser(
+    @Args('password') password: string,
+    @Args('email') email: string,
+    @Args('name') name: string,
+    //@Args({ name: 'age', type: () => Int }) age: number,
+    @Args('age') age: number,
+  ): Promise<User> {
+    return this.usersService.create({ password, email, name, age });
+  }
+
+  @Mutation(() => Boolean)
+  deleteUser(
+    @Args('email') email: string, //
+  ): Promise<boolean> {
+    return this.usersService.delete({ email });
+  }
+
+  @Mutation(() => User, { nullable: true })
+  updateUser(
+    @Args('name') name: string,
+    @Args('age') age: number,
+    @Args('email') email: string,
+    @Args('password') password: string,
+  ): Promise<UpdateResult> {
+    return this.usersService.update({ name, age, email, password });
+  }
+
+  @Query(() => [User], { nullable: true })
+  fetchUsers(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Query(() => User, { nullable: true })
+  fetchUser(@Args('email') email: string): Promise<User> {
+    return this.usersService.findOne({ email });
   }
 }
--- homework/signup/src/app.module.ts
+++ homework/signup/src/app.module.ts
@@ -1,10 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './apis/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
     TypeOrmModule.forRoot({
       type: process.env.DB_TYPE as 'mysql',
       host: process.env.DB_HOST,
