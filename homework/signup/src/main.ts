ee2fbca6b2af9af5c379cc2addb1e6c7de10c2d4
이홍열 <zeler1004@gmail.com>
2023-06-14 16:36:15 +0900

    [add] login-authorization - httpexpectionFilter 관련  파일 및 오탈자에 대한  내용들  일괄변경 동시진행(통신연결 테스트 모두 확인완료)

--- homework/signup/src/main.ts
+++ homework/signup/src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { HttpExeptionFilter } from './commons/filter/http-exception.filter';
+import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 
 const port = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExeptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port, () => {
     console.log(`server listening on port ${port}👌`);
   });
