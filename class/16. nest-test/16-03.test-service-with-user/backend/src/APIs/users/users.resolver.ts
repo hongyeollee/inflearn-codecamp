22e8323b59e758b3a2165a7de493a8d24c89861a
이홍열 <zeler1004@gmail.com>
2023-08-30 23:55:45 +0900

    jest 실습(mocking 사용)

---
+++ class/16. nest-test/16-03.test-service-with-user/backend/src/APIs/users/users.resolver.ts
@@ -0,0 +1,35 @@
+import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { UseGuards } from '@nestjs/common';
+import { IContext } from 'src/commons/interfaces/context';
+import { gqlAuthGuard } from '../auth/guards/gql-auth.guard';
+
+@Resolver()
+export class UsersResolver {
+  constructor(
+    private readonly usersService: UsersService, //
+  ) {}
+
+  @UseGuards(gqlAuthGuard('access'))
+  @Query(() => String)
+  fetchUser(
+    @Context() context: IContext, //
+  ): string {
+    //유저 정보 꺼내오기
+    console.log('===========');
+    console.log(context.req.user);
+    console.log('===========');
+    return '인가에 성공하였습니다.';
+  }
+
+  @Mutation(() => User)
+  createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args({ name: 'age', type: () => Int }) age: number,
+  ): Promise<User> {
+    return this.usersService.create({ email, password, name, age });
+  }
+}
